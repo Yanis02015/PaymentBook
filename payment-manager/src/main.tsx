594c999bffacee8ecff74d4088bb6f2a10c5c28b
Yanis Oulhaci <yaniscrab@gmail.com>
2023-09-05 23:38:53 +0100

    init shadcn/ui

--- payment-manager/src/main.tsx
+++ payment-manager/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./configurations/queryClient";
+import "@/styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
